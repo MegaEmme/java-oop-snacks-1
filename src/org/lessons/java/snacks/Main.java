6f6f368d6f1def78741b0c0b64b7f4bf95f7bc68
MegaEmme <mikmusco@gmail.com>
2025-07-04 13:56:39 +0200

    refactor: comment out snack 2 code for clarity

--- src/org/lessons/java/snacks/Main.java
+++ src/org/lessons/java/snacks/Main.java
@@ -5,26 +5,28 @@ import java.math.RoundingMode;
 
 public class Main {
     public static void main(String[] args) {
+
         // snack 1
+
         // Studente studenteUno = new Studente("Michelangelo", "Musco", 32);
         // System.out.println(studenteUno.getStudente());
 
         // snack 2
-        ContoBancario contoUno = new ContoBancario(12345);
 
-        System.out.println("numero conto: " + contoUno.getNumeroConto());
+        // ContoBancario contoUno = new ContoBancario(12345);
+        // System.out.println("numero conto: " + contoUno.getNumeroConto());
+        // System.out.println("Saldo del conto: " + contoUno.getSaldo().setScale(2) + "
+        // Euro");
+        // contoUno.deposito(BigDecimal.valueOf(1000));
+        // System.out.println(
+        // "Saldo del conto dopo versamento: " + contoUno.getSaldo().setScale(2,
+        // RoundingMode.HALF_UP) + " Euro");
+        // contoUno.prelievo(BigDecimal.valueOf(350));
+        // System.out.println(
+        // "Saldo del conto dopo prelievo: " + contoUno.getSaldo().setScale(2,
+        // RoundingMode.HALF_UP) + " Euro");
 
-        System.out.println("Saldo del conto: " + contoUno.getSaldo().setScale(2) + " Euro");
-
-        contoUno.deposito(BigDecimal.valueOf(1000));
-
-        System.out.println(
-                "Saldo del conto dopo versamento: " + contoUno.getSaldo().setScale(2, RoundingMode.HALF_UP) + " Euro");
-
-        contoUno.prelievo(BigDecimal.valueOf(350));
-
-        System.out.println(
-                "Saldo del conto dopo prelievo: " + contoUno.getSaldo().setScale(2, RoundingMode.HALF_UP) + " Euro");
+        // snack 3
     }
 
 }
